c0bb123437e66be1447bbcd1d19136fb3fbd4edb
DevDrian <matiose12@gmail.com>
2025-06-04 18:14:11 -0500

    Cambiar el nombre de la tabla de 'users' a 'usuarios' en el modelo User y en la migración correspondiente.

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-    
+    protected $table = 'usuarios';
 
     /**
      * The attributes that are mass assignable.
